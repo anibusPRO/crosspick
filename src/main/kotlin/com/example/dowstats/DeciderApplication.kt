43c227205da33f157ab7bbfe44555ec3476d1a78
v.holodov <v.holodov@altuera.com>
2022-11-04 01:37:44 +0300

    Decider 3.0 started

---
+++ src/main/kotlin/com/example/dowstats/DeciderApplication.kt
@@ -0,0 +1,11 @@
+package com.example.dowstats
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DeciderApplication
+
+fun main(args: Array<String>) {
+	runApplication<DeciderApplication>(*args)
+}
